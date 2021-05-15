e6ab1ca70e72ff2874e4e0e029616d39ed3bfab8
Tommy Ho <tommyho@cpp.edu>
2021-05-14 21:43:04 -0700

    modified binarySearch.java

--- NOTES/BinarySearch.java
+++ NOTES/BinarySearch.java
@@ -2,35 +2,60 @@ public class BinarySearch {
     public static void main(String[] args) {
         // goal: find index of number 7 when sorted
         int[] nums = {1, 5, 7, 2, 3, 5, 4, 2, 5, 6, 1, 3, 4}; // should return index of 12, it's last
-        int num = 7;
+        int target = 7;
 
         // sort the array
         sort(nums);
         
         // implement Binary Search
-        System.out.println("Index for " + num + " is: " + binarySearch(nums, num));
+        System.out.println("Index for " + target + " is: " + binarySearch(nums, target));
+        System.out.println("Index for " + target + " is: " + recursiveBinarySearch(nums, target, 0, nums.length-1));
         
     }
 
-    static int binarySearch(int[] arr, int target) {
-        int mid, low, high;
-        low = 0;
-        high = arr.length-1;
+    static int binarySearch(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
 
-        while (high >= low) {
-            // mid = (high+low) / 2;
-            mid = low + (high-low) / 2; // optimal 
-            if (arr[mid] < target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (numbers[mid] == target) {
+                return mid; // target found
+            }
+            // too low, check high partition
+            else if (numbers[mid] < target) {
                 low = mid+1;
             }
-            else if (arr[mid] > target) {
+            // too high, check low partition 
+            else if (numbers[mid] > target) {
                 high = mid-1;
             }
-            else {
-                return mid;
-            }
         }
-        return -1;
+        return -1; // target not found
+    }
+
+    static int recursiveBinarySearch(int[] numbers, int target, int low, int high) {
+        // avoid overflow
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (numbers[mid] == target) {
+            return mid; // target found
+        }
+        // too low, check high partition
+        else if (numbers[mid] < target) {
+            return recursiveBinarySearch(numbers, target, mid+1, high);
+        }
+        // too high, check low partition
+        else if (numbers[mid] > target) {
+            return recursiveBinarySearch(numbers, target, low, mid-1);
+        }
+
+        return -1; // target not found
     }
 
     static void sort(int[] arr) {
